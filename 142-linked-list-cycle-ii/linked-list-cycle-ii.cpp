f959f3c914c9894a51b8f2b7e5d7b22348c1231b
Rishabh Dadhich <rishabhdadhich1805@gmail.com>
2026-08-19 23:20:07 +0530

    Time: 10 ms (44.98%) | Memory: 11.3 MB (55.49%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -13,14 +13,15 @@ public:
         while(fast!=nullptr && fast->next!=nullptr){
             slow=slow->next;
             fast=fast->next->next;
-            if(slow==fast)break;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
+            }
         }
-        if(fast==nullptr || fast->next==nullptr)return nullptr;
-        slow=head;
-        while(slow!=fast){
-            slow=slow->next;
-            fast=fast->next;
-        }
-        return slow;
+        return nullptr;
     }
 };
